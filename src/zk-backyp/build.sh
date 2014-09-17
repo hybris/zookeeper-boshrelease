2384d50464073143b9090ac6e0222a9cce3bfef5
Malte Janduda <mail@janduda.net>
2014-09-17 16:06:11 +0200

    removing zk-backyp source

--- src/zk-backyp/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-go get github.com/mitchellh/goamz/aws
-go get github.com/mitchellh/goamz/s3
-
-go build -o bin/zk-backyp src/zk-backyp.go
--- src/zk-backyp/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-vagrant up
-vagrant ssh -c "sudo bash -c 'mkdir -p /zk-backyp/tmp && cd /zk-backyp && GOPATH=~/.go ./build.sh'"
-vagrant ssh -c "sudo bash -c 'cd /zk-backyp/tmp && /zk-backyp/test/zk-backyp.sh'"
